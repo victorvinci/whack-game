85739368d0163a09aa6eac803a9eb46f2271de1f
victor vinci <victorvinci@protonmail.com>
2019-06-01 17:00:27 +0200

    add a conditional to avoid repeat the same hole

--- whackGame.js
+++ whackGame.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -9,4 +10,10 @@ function randomTime(min, max) {
 function randomHole(holes) {
   const index = Math.floor(Math.random() * holes.length);
   const hole = holes[index];
+  
+  if (hole === lastHole){
+    randomHole(holes);
+  }
+
+  lastHole = hole;
 }

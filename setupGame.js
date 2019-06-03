26f9cbef333beb093cfa9ff9cac04c92186278ae
victor vinci <victorvinci@protonmail.com>
2019-06-03 22:21:08 +0200

    add function for nextLevel (need second look)

--- setupGame.js
+++ setupGame.js
@@ -1,9 +1,22 @@
+let levelIndex = 0;
+
 function loadLevel(){
   if (score === 0) {
     fetch("levelOne.json")
     .then(response => response.json())
     .then(function(data) {
       const game = document.querySelector('.game');
-      game.innerHTML = data.levels[0].levelOne.html;
+      game.innerHTML = data.levels[levelIndex].level.html;
+    });
+  }}
+
+function nextLevel() {
+  if (score >= 10) {
+    levelIndex++
+    fetch("levelOne.json")
+    .then(response => response.json())
+    .then(function(data) {
+      const game = document.querySelector('.game');
+      game.innerHTML = data.levels[levelIndex].level.html;
     });
   }}
